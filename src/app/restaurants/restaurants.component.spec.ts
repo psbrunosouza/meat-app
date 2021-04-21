009fcada1839787297e04babbca571aeda7505e7
psbrunosouza <psbrunosouza@gmail.com>
2021-04-21 08:21:19 -0300

    update

--- src/app/restaurants/restaurants.component.spec.ts
+++ src/app/restaurants/restaurants.component.spec.ts
@@ -1,14 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import {RestaurantsComponent} from './restaurants.component';
 import {RestaurantsService} from './restaurants.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Restaurant } from './restaurant/restaurant.model';
+import { MEAT_API } from '../app.api';
 
 let component: RestaurantsComponent;
 let fixture: ComponentFixture<RestaurantsComponent>;
-let service: RestaurantsService;
-let http: HttpClient;
 let mockDb: Restaurant[] = [
   {
     "id": "bread-bakery",
@@ -31,25 +29,35 @@ describe('RestaurantsComponent', () => {
       ],
       providers: [
         RestaurantsService,
-
       ],
       imports: [
-        HttpClientModule,
         HttpClientTestingModule,
-        HttpClient
       ]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(RestaurantsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
   });
 
-  beforeAll(() => {
-    http = TestBed.inject(HttpClient);
+  it('should get all restaurants', () => {
+    let http = TestBed.inject(HttpTestingController);
+    let service = TestBed.inject(RestaurantsService);
+    let listRestaurants: Restaurant[] = [];
+
+    service.restaurants().subscribe(
+      restaurants => {
+        listRestaurants = restaurants;
+        console.log(listRestaurants);
+        console.log(mockDb);
+      }
+    );
+
+    // let request = http.expectOne({method: 'GET', url: `${MEAT_API}/restaurants`});
+    // request.flush('Get')
+    expect(listRestaurants).toEqual(mockDb);
   });
 
   // xit('should create', () => {
@@ -70,19 +78,5 @@ describe('RestaurantsComponent', () => {
   //   }
   // });
 
-  it('should get all restaurants', async (done: DoneFn) => {
-    let listRestaurants: Restaurant[] = [];
-    service = new RestaurantsService(http);
-
-    await service.restaurants().subscribe(
-      restaurants => {
-        listRestaurants = restaurants;
-        console.log(listRestaurants);
-        console.log(mockDb);
-      }
-    );
-
-    expect(listRestaurants).toEqual(mockDb);
-  });
 
 });
