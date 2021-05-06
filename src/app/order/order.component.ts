f7349b3209c2b63800705eed72d2653e6f4d3ef5
psbrunosouza <psbrunosouza@gmail.com>
2021-05-05 22:37:00 -0300

    order implemented

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from '../shared/option/radio-option.model';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.css']
+  styleUrls: ['./order.component.css'],
 })
 export class OrderComponent implements OnInit {
+  paymentOptions: RadioOption[] = [
+    { label: 'Dinheiro', value: 'MON' },
+    { label: 'Débito', value: 'DEB' },
+    { label: 'Cartão refeição', value: 'REF' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shared/option/option.component.ts
+++ src/app/shared/option/option.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RadioOption } from './radio-option.model';
 
 @Component({
   selector: 'mt-option',
   templateUrl: './option.component.html',
-  styleUrls: ['./option.component.css']
+  styleUrls: ['./option.component.css'],
 })
 export class OptionComponent implements OnInit {
+  @Input() options: RadioOption[];
+  value: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  setValue(value: any) {
+    this.value = value;
   }
-
 }
