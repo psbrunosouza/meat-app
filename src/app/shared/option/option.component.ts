a8603c177e58f074ab94d65b918a811cec2572f5
psbrunosouza <psbrunosouza@gmail.com>
2021-05-05 23:10:17 -0300

    control value accessor

--- src/app/shared/option/option.component.ts
+++ src/app/shared/option/option.component.ts
@@ -1,14 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
 import { RadioOption } from './radio-option.model';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'mt-option',
   templateUrl: './option.component.html',
   styleUrls: ['./option.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => OptionComponent ),
+      multi: true
+    }
+  ]
 })
-export class OptionComponent implements OnInit {
+export class OptionComponent implements OnInit, ControlValueAccessor {
   @Input() options: RadioOption[];
   value: any;
+  onChange: any;
 
   constructor() {}
 
@@ -16,5 +25,22 @@ export class OptionComponent implements OnInit {
 
   setValue(value: any) {
     this.value = value;
+    this.onChange(this.value);
+  }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
   }
 }
